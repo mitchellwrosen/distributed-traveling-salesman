b5b3056a39542fc1ce578dc0faea35a030347f5c
Mitchell Rosen <mitchellwrosen@gmail.com>
2013-11-23 11:40:33 -0800

    implemented op_minpath

--- src/naive/distributed/op_minpath.cpp
+++ src/naive/distributed/op_minpath.cpp
@@ -2,6 +2,8 @@
 
 #include <mpi.h>
 
+#include "common/path.h"
+
 // static
 MPI_Op _op_minpath::op_;
 
@@ -12,12 +14,20 @@ bool _op_minpath::initialized_ = false;
 MPI_Op _op_minpath::getOp() {
   if (initialized_)
     return op_;
-  
+
   MPI_Op_create(opImpl, true, &op_);
   initialized_ = true;
+  return op_;
 }
 
 // static
 void _op_minpath::opImpl(void* in, void* inout, int* len, MPI_Datatype* datatype) {
-  
+  Path pathOne = Path::deserialize((char*) in);
+  Path pathTwo = Path::deserialize((char*) inout);
+
+  // Replace "inout" if necessary, which is when "in" is a shorter path.
+  if (pathOne.cost() < pathTwo.cost()) {
+    char* serialized = pathOne.serialize();
+    memcpy(inout, serialized, pathOne.serializedLen());
+  }
 }
